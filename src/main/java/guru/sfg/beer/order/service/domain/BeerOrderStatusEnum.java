3da54b82bc4461985343b300c8df3fea1522cff8
Valeri Totev <72451048+vtotev@users.noreply.github.com>
2024-02-04 14:21:20 +0200

    Added enumerations for Spring State Machine

---
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderStatusEnum.java
@@ -0,0 +1,6 @@
+package guru.sfg.beer.order.service.domain;
+
+public enum BeerOrderStatusEnum {
+    NEW, VALIDATED, VALIDATION_EXCEPTION, ALLOCATED, ALLOCATION_EXCEPTION,
+    PENDING_INVENTORY, PICKED_UP, DELIVERED, DELIVERY_EXCEPTION
+}
